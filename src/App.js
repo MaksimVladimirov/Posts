8259be7b7654b438b23fce3128af17d2611fa83a
Maksim Vladimirov <vl4dimirov@gmail.com>
2022-11-20 10:33:30 +0300

    changing-pages

--- src/App.js
+++ src/App.js
@@ -9,16 +9,25 @@ import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  let pagesArray = getPagesArray(totalPages);
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
   });
 
   useEffect(() => {
@@ -63,6 +72,11 @@ function App() {
           title="Посты про JS"
         />
       )}
+      <div className="page__wrapper">
+        {pagesArray.map((p) => (
+          <span className="page">{p}</span>
+        ))}
+      </div>
     </div>
   );
 }
